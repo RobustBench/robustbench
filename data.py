8ff7abf3a839cd0f7dd0ecb2d06729cb137123e0
Maksym Andriushchenko <m.andriushchenko@gmail.com>
2020-08-27 21:50:26 +0200

    speed up data loading a bit

--- data.py
+++ data.py
@@ -5,11 +5,20 @@ import torchvision.transforms as transforms
 
 
 def load_cifar10(n_examples, data_dir='./data'):
+    batch_size = 100
     transform_chain = transforms.Compose([transforms.ToTensor()])
     item = datasets.CIFAR10(root=data_dir, train=False, transform=transform_chain, download=True)
-    test_loader = data.DataLoader(item, batch_size=1000, shuffle=False, num_workers=0)
+    test_loader = data.DataLoader(item, batch_size=batch_size, shuffle=False, num_workers=0)
 
-    x_test = torch.cat([x for (x, y) in test_loader], 0)[:n_examples].cuda()
-    y_test = torch.cat([y for (x, y) in test_loader], 0)[:n_examples].cuda()
+    x_test, y_test = [], []
+    for i, (x, y) in enumerate(test_loader):
+        x_test.append(x)
+        y_test.append(y)
+        if batch_size * i >= n_examples:
+            break
 
-    return x_test, y_test
+    x_test = torch.cat(x_test)[:n_examples]
+    y_test = torch.cat(y_test)[:n_examples]
+
+    return x_test.cuda(), y_test.cuda()
+
