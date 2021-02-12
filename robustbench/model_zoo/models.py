6425435260ce3c090e7b2ff79a9c0b58e5f5d5e5
Edoardo Debenedetti <edoardo.m.debenedetti@gmail.com>
2021-02-12 10:42:10 +0100

    Fix OrderedDict type

--- robustbench/model_zoo/models.py
+++ robustbench/model_zoo/models.py
@@ -1,12 +1,12 @@
 from collections import OrderedDict
-from typing import Any, Dict, OrderedDict as OrderedDictType
+from typing import Any, Dict
 
 from robustbench.model_zoo.cifar10 import cifar_10_models
 from robustbench.model_zoo.enums import BenchmarkDataset, ThreatModel
 
-ModelsDict = OrderedDictType[str, Dict[str, Any]]
-ThreatModelsDict = OrderedDictType[ThreatModel, ModelsDict]
-BenchmarkDict = OrderedDictType[BenchmarkDataset, ThreatModelsDict]
+ModelsDict = Dict[str, Dict[str, Any]]
+ThreatModelsDict = Dict[ThreatModel, ModelsDict]
+BenchmarkDict = Dict[BenchmarkDataset, ThreatModelsDict]
 
 model_dicts: BenchmarkDict = OrderedDict([
     (BenchmarkDataset.cifar_10, cifar_10_models)
