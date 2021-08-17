fb6479d51fd6890abe07e53c4c10902277feeb0c
Edoardo Debenedetti <edoardo.m.debenedetti@gmail.com>
2021-08-17 21:47:24 +0100

    Add new columns for external evals and flags

--- setup.py
+++ setup.py
@@ -14,12 +14,12 @@ setuptools.setup(
     url="https://github.com/RobustBench/robustbench",
     packages=setuptools.find_packages(),
     install_requires=[
-        'requests~=2.23.0',
+        'requests~=2.25.0',
         'numpy~=1.19.4',
         'Jinja2~=2.11.2',
         'tqdm~=4.56.1',
         'pandas~=1.1.0',
-        'autoattack @ git+https://github.com/fra31/auto-attack.git@9b264b52bb65c373373727f532865a5551ab9c02#egg=autoattack'
+        'autoattack @ git+https://github.com/fra31/auto-attack.git@f551404cf7ed1d6e17d3041d5709ad23f6dbf352#egg=autoattack'
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
