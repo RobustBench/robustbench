cda90358de89c367b8cb71d5eab1e9d9c3bb6640
Maksym Andriushchenko <m.andriushchenko@gmail.com>
2021-08-28 13:32:06 +0200

    update jsons to include the flag values

--- tests/config.py
+++ tests/config.py
@@ -1,7 +1,7 @@
 def get_test_config():
     config = {
         'batch_size': 128,
-        'datasets': ['imagenet'],  #['cifar10', 'cifar100', 'imagenet'],
+        'datasets': ['cifar10', 'cifar100', 'imagenet'],
         'threat_models': ['Linf', 'L2', 'corruptions'],
         'data_dir': 'data',
         'imagenet_data_dir': '/tmldata1/andriush/imagenet',
