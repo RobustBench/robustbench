81d70a6921790a58bfaecfb218ea04e657bd3b98
fra31 <francesco91.croce@gmail.com>
2022-02-14 13:14:08 +0000

    add Kang2021Stable model

---
+++ robustbench/model_zoo/architectures/sodef_layers.py
@@ -0,0 +1,143 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import geotorch
+from torch.nn.parameter import Parameter
+
+from robustbench.model_zoo.architectures.dm_wide_resnet import CIFAR10_MEAN, CIFAR10_STD, \
+    DMWideResNet, Swish
+
+
+class Identity(nn.Module):
+    def __init__(self):
+        super(Identity, self).__init__()
+        
+    def forward(self, x):
+        return x
+    
+from torchdiffeq import odeint_adjoint as odeint
+
+
+class ConcatFC(nn.Module):
+
+    def __init__(self, dim_in, dim_out):
+        super(ConcatFC, self).__init__()
+        self._layer = nn.Linear(dim_in, dim_out)
+    def forward(self, t, x):
+        return self._layer(x)
+
+
+class ODEfunc_mlp(nn.Module): #resnet4/dense1
+
+    def __init__(self, dim):
+        super(ODEfunc_mlp, self).__init__()
+        self.fc1 = ConcatFC(64, 64)
+        self.act1 = torch.sin 
+        self.nfe = 0
+
+    def forward(self, t, x):
+        self.nfe += 1
+        out = -1*self.fc1(t, x)
+        out = self.act1(out)
+        return out
+
+
+class ODEBlock(nn.Module):
+
+    def __init__(self, odefunc):
+        super(ODEBlock, self).__init__()
+        self.odefunc = odefunc
+        self.integration_time = torch.tensor([0, 5]).float()
+
+    def forward(self, x):
+        self.integration_time = self.integration_time.type_as(x)
+        out = odeint(self.odefunc, x, self.integration_time, rtol=1e-3, atol=1e-3)
+        return out[1]
+
+    @property
+    def nfe(self):
+        return self.odefunc.nfe
+
+    @nfe.setter
+    def nfe(self, value):
+        self.odefunc.nfe = value
+
+
+class MLP_OUT_ORTH1024(nn.Module):
+    def __init__(self):
+        super(MLP_OUT_ORTH1024, self).__init__()
+        self.fc0 = ORTHFC(1024, 64, False)
+    def forward(self, input_):
+        h1 = self.fc0(input_)
+        return h1
+
+
+class newLinear(nn.Module):
+    def __init__(self, in_features, out_features, bias=True):
+        super(newLinear, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = Parameter(torch.Tensor(in_features,out_features))
+#         self.weight = self.weighttemp.T
+        if bias:
+            self.bias = Parameter(torch.Tensor(out_features))
+        else:
+            self.register_parameter('bias', None)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        if self.bias is not None:
+            fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
+            bound = 1 / math.sqrt(fan_in)
+            nn.init.uniform_(self.bias, -bound, bound)
+
+    def forward(self, input):
+        return F.linear(input, self.weight.T, self.bias)
+
+    def extra_repr(self) -> str:
+        return 'in_features={}, out_features={}, bias={}'.format(
+            self.in_features, self.out_features, self.bias is not None
+        )
+
+
+class ORTHFC(nn.Module):
+    def __init__(self, dimin, dimout, bias):
+        super(ORTHFC, self).__init__()
+        if dimin >= dimout:
+            self.linear = newLinear(dimin, dimout,  bias=bias)
+        else:
+            self.linear = nn.Linear(dimin, dimout,  bias=bias)
+        geotorch.orthogonal(self.linear, "weight")
+
+    def forward(self, x):
+        return self.linear(x)
+    
+    
+class MLP_OUT_LINEAR(nn.Module):
+    def __init__(self):
+        super(MLP_OUT_LINEAR, self).__init__()
+        self.fc0 = nn.Linear(64, 10)
+    def forward(self, input_):
+        h1 = self.fc0(input_)
+        return h1
+        
+
+def rebuffi_sodef():
+    model = DMWideResNet(num_classes=10,
+                         depth=70,
+                         width=16,
+                         activation_fn=Swish,
+                         mean=CIFAR10_MEAN,
+                         std=CIFAR10_STD)
+    model.logits = Identity()
+    fc_features = MLP_OUT_ORTH1024()
+    odefunc = ODEfunc_mlp(0)
+    odefeature_layers = ODEBlock(odefunc)
+    odefc_layers = MLP_OUT_LINEAR()
+    model_dense = nn.Sequential( odefeature_layers, odefc_layers)
+    new_model = nn.Sequential(model, fc_features, model_dense)
+    
+    return new_model
+
