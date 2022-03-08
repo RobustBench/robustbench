08d20a273f16596159e8e7476d987202fdbbb833
Maksym Andriushchenko <m.andriushchenko@gmail.com>
2022-03-08 17:52:51 +0100

    add extra dependencies in setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     url="https://github.com/RobustBench/robustbench",
     packages=setuptools.find_packages(),
     install_requires=[
-        'torch>=1.7.1', 'torchvision>=0.8.2', 'requests~=2.25.0',
+        'torch>=1.7.1', 'torchvision>=0.8.2', 'torchdiffeq', 'geotorch', 'requests~=2.25.0',
         'numpy>=1.19.4', 'Jinja2~=2.11.2', 'tqdm~=4.56.1', 'pandas~=1.2.0',
         'autoattack @ git+https://github.com/fra31/auto-attack.git@6482e4d6fbeeb51ae9585c41b16d50d14576aadc#egg=autoattack'
     ],
